84115dc920971a628eb788306c5cf0376e55641d
JustRock-star <arman.grigoryan.ithink@gmail.com>
2025-10-20 14:06:05 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
-// ВСТАВЬ СЮДА firebaseConfig из консоли Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyBGYzw0OqCKqHOjQed1MeThpP2xQl3fNq4",
+  authDomain: "fake-ai-demo.firebaseapp.com",
+  projectId: "fake-ai-demo",
+  storageBucket: "fake-ai-demo.appspot.com",
+  messagingSenderId: "289732972043",
+  appId: "1:289732972043:web:0af9bc1cdc5d8ba122c10e"
 };
 
 if (!window.firebaseConfig) {
